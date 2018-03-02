61ef615b36c560c2f3bd50f7d0b7cdde912b4e96
张保林 <baolin.zhang@socialcredits.cn>
2018-03-02 10:22:43 +0800

    add server

--- blockchain.py
+++ blockchain.py
@@ -5,15 +5,54 @@
  * Created by zbl on 18-3-2 上午9:28.
 """
 
+import json
+import hashlib
+from time import time
+
 
 class BlockChain(object):
+
+    # block = {
+    #     'index': 1,
+    #     'timestamp': 1506057125.900785,
+    #     'transactions': [
+    #         {
+    #             'sender': "8527147fe1f5426f9dd545de4b27ee00",
+    #             'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+    #             'amount': 5,
+    #         }
+    #     ],
+    #     'proof': 324984774000,
+    #     'previous_hash': "xxxxxxxxxxxxxxxx"
+    # }
     
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-        
-    def new_block(self):
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        生成新块
+        :param proof: <int> The proof given by the Proof of Work algorithm
+        :param previous_hash: (Optional) <str> Hash of previous Block
+        :return: <dict> New Block
+        """
+    
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+    
+        # Reset the current list of transactions
+        self.current_transactions = []
+    
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -34,8 +73,45 @@ class BlockChain(object):
     
     @staticmethod
     def hash(block):
-        pass
+        """
+        生成块的 SHA-256 hash值
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # We must make sure that the Dictionary is Ordered,
+        # or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
     
     @property
     def last_block(self):
-        pass
+        return self.chain[-1]
+    
+    def poof_of_word(self, last_proof):
+        """
+        简单的工作量证明:
+        - 查找一个 p' 使得 hash(pp') 以4个0开头
+        - p 是上一个块的证明,  p' 是当前的证明
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+    
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        验证证明: 是否hash(last_proof, proof)以4个0开头?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+    
+        guess = '{0}{1}'.format(last_proof, proof).encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
---
+++ server.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ * Created by zbl on 18-3-2 上午10:05.
+"""
+
+
+
